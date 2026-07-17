f9522344af6e80f74540841d4bcdff4b3355ef55
Merlinna Line <7merlinna7@gmail.com>
2026-07-18 00:39:08 +0700

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private static string HorizontalAxdisName = "Horizontal";
+    private static string VerticalAxisName = "Vertical";
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private float _moveSpeed;
+
+    private void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw(HorizontalAxdisName), 0, Input.GetAxisRaw(VerticalAxisName));
+        Vector3 normalizedInput = input.normalized;
+        ProcessMoveTo(normalizedInput);
+    }
+    private void ProcessMoveTo(Vector3 direction) => _characterController.Move(direction * _moveSpeed * Time.deltaTime);
+}
